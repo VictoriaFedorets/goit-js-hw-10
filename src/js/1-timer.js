7086f638ef89d6724473884eb354bedb44df0c1e
Victoria <vika96karnauhova@gmail.com>
2024-06-26 18:45:23 +0300

    dfgh

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,11 +1,3 @@
-// Описаний в документації
-import flatpickr from 'flatpickr';
-// Додатковий імпорт стилів
-import 'flatpickr/dist/flatpickr.min.css';
-// Описаний у документації
-import iziToast from 'izitoast';
-// Додатковий імпорт стилів
-import 'izitoast/dist/css/iziToast.min.css';
 
 const elements = {
     inputDate: document.querySelector('#datetime-picker');
